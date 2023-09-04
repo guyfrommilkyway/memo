115f7656ca8f9f5381e954b374c3cb8c23567e58
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:46:49 +0800

    Add Trash page component

---
+++ src/pages/Trash/index.tsx
@@ -0,0 +1,16 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const Trash: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='Trash' />
+      <h1>Trash</h1>
+    </Fragment>
+  )
+}
+
+export default Trash;
