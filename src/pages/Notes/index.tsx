4bce805365274da290e04cad91f3f1c579a80188
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:46:38 +0800

    Add Notes page component

---
+++ src/pages/Notes/index.tsx
@@ -0,0 +1,16 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const Notes: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='React Notes' />
+      <h1>Notes</h1>
+    </Fragment>
+  )
+}
+
+export default Notes;
