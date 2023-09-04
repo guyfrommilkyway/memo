edca88b93be0d692263b646a14fa557b2a7de558
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:01:51 +0800

    Setup Redux, Chakra UI, Suspense fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+// import packages below
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
+
+// import components below
+import App from './app/App.tsx'
+
+// import helpers below
+import { store } from './app/store'
+
+// import assets below
+import '@/assets/styles/globals.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ChakraProvider>
+        <Suspense fallback={<span>Loading...</span>}>
+          <App />
+        </Suspense>
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
