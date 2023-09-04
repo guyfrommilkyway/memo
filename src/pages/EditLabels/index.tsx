a18099b6a84a5c8b652d77f3d1fd6ae60e373b5a
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:46:28 +0800

    Add Edit Labels page component

---
+++ src/pages/EditLabels/index.tsx
@@ -0,0 +1,16 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const EditLabels: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='Edit Labels' />
+      <h1>Edit Labels</h1>
+    </Fragment>
+  )
+}
+
+export default EditLabels;
