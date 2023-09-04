e5a6f4cc0eefb632e28ed9df11369daa8dec2a0f
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:46:19 +0800

    Add Archive page component

---
+++ src/pages/Archive/index.tsx
@@ -0,0 +1,16 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const Archive: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='Archive' />
+      <h1>Archive</h1>
+    </Fragment>
+  )
+}
+
+export default Archive;
