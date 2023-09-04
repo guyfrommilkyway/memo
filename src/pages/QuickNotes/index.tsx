86eb6f30c77d14cfab9095434e96f80ffba43724
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:47:05 +0800

    Add Quick Notes page component

---
+++ src/pages/QuickNotes/index.tsx
@@ -0,0 +1,15 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const QuickNotes: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='Quick Notes' />
+      <h1>Quick Notes</h1>
+    </Fragment>)
+}
+
+export default QuickNotes;
