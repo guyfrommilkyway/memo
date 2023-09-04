faac27a4efe06e017143480dd2bde4848cc5bc3e
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:46:57 +0800

    Add Reminders page component

---
+++ src/pages/Reminders/index.tsx
@@ -0,0 +1,16 @@
+// import packages below
+import React, { Fragment } from 'react';
+
+// import components below
+import Head from '@/common/components/Head';
+
+const Reminders: React.FC = () => {
+  return (
+    <Fragment>
+      <Head title='Reminders' />
+      <h1>Reminders</h1>
+    </Fragment>
+  )
+}
+
+export default Reminders;
