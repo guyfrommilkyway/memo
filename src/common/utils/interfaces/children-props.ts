2c89d80f6827c250f0f5e53aadd22374638d92b1
Almer Tampus <almerflorestampus@gmail.com>
2023-09-04 08:47:11 +0800

    Add interfaces

---
+++ src/common/utils/interfaces/children-props.ts
@@ -0,0 +1,6 @@
+// import packages below
+import { ReactNode } from 'react';
+
+export interface ChildrenProps {
+  children: string | JSX.Element | JSX.Element[] | ReactNode;
+}
---
+++ src/common/utils/interfaces/meta-props.ts
@@ -0,0 +1,6 @@
+export interface MetaProps {
+  title: string;
+  author?: string;
+  description?: string;
+  keywords?: string;
+}
