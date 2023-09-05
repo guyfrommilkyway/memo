ddcce68b31cd7e0d5a2be2e2e04f431ca272eb61
Almer Tampus <almerflorestampus@gmail.com>
2023-09-05 23:44:02 +0800

    Integrate react-helmet & redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 import { ChakraProvider } from '@chakra-ui/react'
+import { HelmetProvider } from 'react-helmet-async';
 
 // import components below
 import App from './app/App.tsx'
 
 // import helpers below
-import { store } from './app/store'
+import { store, persistor } from './app/store'
 
 // import assets below
 import '@/assets/styles/globals.scss'
@@ -16,11 +18,15 @@ import '@/assets/styles/globals.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <Suspense fallback={<span>Loading...</span>}>
-          <App />
-        </Suspense>
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <Suspense fallback={<span>Loading...</span>}>
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
+          </Suspense>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
