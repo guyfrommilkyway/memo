00059f2f5958f909ebe3498ef948805b866f97c5
Almer Tampus <almerflorestampus@gmail.com>
2023-09-05 23:44:27 +0800

    Configure routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 // import packages below
-import Layout from '@/common/components/Layout';
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // import components below
+import PublicRoute from '@/common/components/PublicRoute';
+import ProtectedRoute from '@/common/components/ProtectedRoute';
+import Layout from '@/common/components/Layout';
 import Notes from '@/pages/Notes';
 import Reminders from '@/pages/Reminders';
 import QuickNotes from '@/pages/QuickNotes';
@@ -11,42 +13,27 @@ import EditLabels from '@/pages/EditLabels';
 import Archive from '@/pages/Archive';
 import Trash from '@/pages/Trash';
 
-// router
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Notes />
-      },
-      {
-        path: 'reminders',
-        element: <Reminders />
-      },
-      {
-        path: 'quick-notes',
-        element: <QuickNotes />
-      },
-      {
-        path: 'edit-labels',
-        element: <EditLabels />
-      },
-      {
-        path: 'archive',
-        element: <Archive />
-      },
-      {
-        path: 'trash',
-        element: <Trash />
-      },
-    ]
-  }
-]);
-
 const App: React.FC = () => {
-  return <RouterProvider router={router} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path='/login' element={<div>Login Page</div>} />
+          <Route path='/sign-up' element={<div>Sign Up Page</div>} />
+        </Route>
+        <Route element={<ProtectedRoute />} >
+          <Route element={<Layout />}>
+            <Route path='/' element={<Notes />} />
+            <Route path='/reminders' element={<Reminders />} />
+            <Route path='/quick-notes' element={<QuickNotes />} />
+            <Route path='/edit-labels' element={<EditLabels />} />
+            <Route path='/archive' element={<Archive />} />
+            <Route path='/trash' element={<Trash />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App
+export default App;
