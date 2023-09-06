36a00a9d742106c37fb13bbc377d89226c55c310
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:48:26 +0800

    Add convertToEditorState helper function

---
+++ src/common/helpers/convertToEditorState.ts
@@ -0,0 +1,11 @@
+// import packages below
+import { EditorState, RawDraftContentState, convertFromRaw } from 'draft-js';
+
+const convertToEditorState = (raw: RawDraftContentState) => {
+  const convertedState = convertFromRaw(raw);
+  const editorState = EditorState.createWithContent(convertedState);
+
+  return editorState;
+};
+
+export default convertToEditorState;
