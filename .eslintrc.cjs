cab1711314222a9d820f1ada539af5174b2ace92
Almer Tampus <almerflorestampus@gmail.com>
2023-09-06 08:29:24 +0800

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-first-prop-new-line': 0,
+    'react/display-name': 0,
+    'react/jsx-closing-bracket-location': 1,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
