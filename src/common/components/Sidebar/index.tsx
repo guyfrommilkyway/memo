f572c40d28bf7c48c8ce0923d4be5a647b17452e
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:50:52 +0800

    Minor UI update

--- src/common/components/Sidebar/index.tsx
+++ src/common/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 // import packages below
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 // import components below
 const Navbar = React.lazy(() => import('./components/Navbar'))
@@ -7,7 +8,9 @@ const Navbar = React.lazy(() => import('./components/Navbar'))
 const Sidebar: React.FC = () => {
   return (
     <section className='main__sidebar'>
-      <Navbar />
+      <Box py={8}>
+        <Navbar />
+      </Box>
     </section>
   )
 }
