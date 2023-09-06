ab526ccbc3e36ea5a57dd6460241de1b4839cebc
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:50:41 +0800

    Add prop types validation

--- src/common/components/Modal/index.tsx
+++ src/common/components/Modal/index.tsx
@@ -6,38 +6,35 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton
-} from '@chakra-ui/react'
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
 // import utils below
 import { ModalProps } from '@/common/utils/types';
 
-const CustomModal: React.FC<ModalProps> = (props) => {
+const CustomModal: React.FC<ModalProps> = props => {
   const { header, body, footer, isOpen, onClose } = props;
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal size='xl' isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent pt={8}>
-        {header && (
-          <ModalHeader>
-            {header}
-          </ModalHeader>
-        )}
+        {header && <ModalHeader>{header}</ModalHeader>}
         <ModalCloseButton />
-        {body && (
-          <ModalBody py={4}>
-            {body}
-          </ModalBody>
-        )}
-        {footer && (
-          <ModalFooter>
-            {footer}
-          </ModalFooter>
-        )}
+        {body && <ModalBody py={4}>{body}</ModalBody>}
+        {footer && <ModalFooter>{footer}</ModalFooter>}
       </ModalContent>
-    </Modal >
-  )
-}
+    </Modal>
+  );
+};
 
 export default CustomModal;
+
+CustomModal.propTypes = {
+  header: PropTypes.element,
+  body: PropTypes.element,
+  footer: PropTypes.element,
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
