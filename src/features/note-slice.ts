0871793375c77470bcdfbe274e1f1760534d8bf1
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:51:20 +0800

    Code refactoring of action creators

--- src/features/note-slice.ts
+++ src/features/note-slice.ts
@@ -1,8 +1,10 @@
 // import packages below
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
 // import utils below
 import { Notes } from '@/common/utils/note-types';
+import { getCurrentDT } from '@/common/utils/getCurrentDT';
 
 const initialState: Notes = {
   notes: [],
@@ -13,13 +15,20 @@ const notesSlice = createSlice({
   initialState,
   reducers: {
     create(state, action) {
-      state.notes = [...state.notes, { ...action.payload, id: new Date() }];
+      state.notes = [
+        { ...action.payload, id: uuidv4(), dateCreated: getCurrentDT(), dateUpdated: getCurrentDT() },
+        ...state.notes,
+      ];
     },
     update(state, action) {
-      state.notes = state.notes.map((note) => (action.payload.id === note.id ? action.payload : note));
+      const newState = state.notes.map(note =>
+        action.payload.id === note.id ? { ...action.payload, dateUpdated: getCurrentDT() } : note,
+      );
+
+      state.notes = newState;
     },
     remove(state, action) {
-      state.notes = state.notes.filter((note) => action.payload !== note.id);
+      state.notes = state.notes.filter(note => action.payload !== note.id);
     },
   },
 });
