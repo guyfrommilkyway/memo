6e0729b75457c6fed7aa3a0e0d9b53f0e953a5f6
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:46:07 +0800

    Integrate LoaderScreen component

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
 // import packages below
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
 
 // import components below
-import App from './app/App.tsx'
+import App from './app/App.tsx';
+import LoaderScreen from '@/common/components/LoaderScreen';
 
 // import helpers below
-import { store, persistor } from './app/store'
+import { store, persistor } from './app/store';
 
 // import assets below
-import '@/assets/styles/globals.scss'
+import '@/assets/styles/globals.scss';
+import '@/assets/vendors/react-draft-wysiwyg.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<LoaderScreen />} persistor={persistor}>
         <ChakraProvider>
-          <Suspense fallback={<span>Loading...</span>}>
+          <Suspense fallback={<LoaderScreen />}>
             <HelmetProvider>
               <App />
             </HelmetProvider>
@@ -29,4 +31,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-)
+);
