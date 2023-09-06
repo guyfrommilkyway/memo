b3544368db497fad1bccc75f8214b1f4a9163b56
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 03:50:00 +0800

    Minor UI updates

--- src/common/components/Content/index.tsx
+++ src/common/components/Content/index.tsx
@@ -2,12 +2,19 @@
 import React from 'react';
 
 // import utils below
-import { ChildrenProps } from '@/common/utils/interfaces/children-props';
+import { ChildrenProps } from '@/common/utils/types';
+import { Box } from '@chakra-ui/react';
 
 const Content: React.FC<ChildrenProps> = (props) => {
   const { children } = props;
 
-  return <section className='main__content'>{children}</section>
+  return (
+    <section className='main__content'>
+      <Box p={8}>
+        {children}
+      </Box>
+    </section>
+  )
 }
 
 export default Content;
--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -1,8 +1,20 @@
 // import packages below
 import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
 
 const Footer: React.FC = () => {
-  return <footer className='main__footer'>Footer</footer>
-}
+  return (
+    <footer className='main__footer'>
+      <Flex direction='column' justify='center' alignItems='center' gap={0} h='100%'>
+        <Text textAlign='center' fontSize={14}>
+          Copyright &copy; Almer Tampus 2023
+        </Text>
+        <Text textAlign='center' fontSize={14}>
+          All rights reserved
+        </Text>
+      </Flex>
+    </footer>
+  );
+};
 
 export default Footer;
