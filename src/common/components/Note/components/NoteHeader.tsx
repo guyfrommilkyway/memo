b1856e7b6aa0a8f4796dffe1d37e198b8dbad2bc
Almer Tampus <almerflorestampus@gmail.com>
2023-09-07 05:35:51 +0800

    Minor UI update

--- src/common/components/Note/components/NoteHeader.tsx
+++ src/common/components/Note/components/NoteHeader.tsx
@@ -1,6 +1,6 @@
 // import packages below
 import React, { memo } from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 // import utils below
@@ -10,8 +10,10 @@ const NoteHeader: React.FC<NoteHeaderProps> = memo(props => {
   const { onOpen } = props;
 
   return (
-    <Box w='100%' maxW={620} mx='auto' border='1px solid red'>
-      <Button onClick={onOpen}>Open</Button>
+    <Box w='100%' maxW={620} mx='auto' border='1px solid #67798E' borderRadius={8}>
+      <Text p={4} cursor='pointer' onClick={onOpen}>
+        Take a note...
+      </Text>
     </Box>
   );
 });
