c9dc167014ec16d629e810b99e26ecab5491eb71
Almer Tampus <almerflorestampus@gmail.com>
2023-11-11 08:55:04 +0800

    Remove lazy loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,54 +1,49 @@
-// import packages below
+// packages
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-// import components below
+// components
 import PublicRoute from '@/components/PublicRoute';
 import ProtectedRoute from '@/components/ProtectedRoute';
 import Layout from '@/components/Layout';
-import Notes from '@/pages/Notes';
-const Login = React.lazy(() => {
-  return Promise.all([import('@/pages/Login'), new Promise(resolve => setTimeout(resolve, 300))]).then(
-    ([moduleExports]) => moduleExports,
-  );
-});
-const SignUp = React.lazy(() => {
-  return Promise.all([import('@/pages/SignUp'), new Promise(resolve => setTimeout(resolve, 300))]).then(
-    ([moduleExports]) => moduleExports,
-  );
-});
-const Archive = React.lazy(() => {
-  return Promise.all([import('@/pages/Archive'), new Promise(resolve => setTimeout(resolve, 300))]).then(
-    ([moduleExports]) => moduleExports,
-  );
-});
-const Trash = React.lazy(() => {
-  return Promise.all([import('@/pages/Trash'), new Promise(resolve => setTimeout(resolve, 300))]).then(
-    ([moduleExports]) => moduleExports,
-  );
-});
-const NotFound = React.lazy(() => {
-  return Promise.all([import('@/pages/NotFound'), new Promise(resolve => setTimeout(resolve, 300))]).then(
-    ([moduleExports]) => moduleExports,
-  );
-});
+import NotesPage from '@/pages/Notes';
+import LoginPage from '@/pages/Login';
+import ArchivePage from '@/pages/Archive';
+import TrashPage from '@/pages/Trash';
+import NotFoundPage from '@/pages/NotFound';
 
 const App: React.FC = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<NotFound />} />
-        <Route element={<PublicRoute />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/sign-up' element={<SignUp />} />
+        <Route
+          element={
+            <PublicRoute
+              isAuthenticated={isAuthenticated}
+              isLoading={isLoading}
+            />
+          }
+        >
+          <Route path='/login' element={<LoginPage />} />
         </Route>
-        <Route element={<ProtectedRoute />}>
+        <Route
+          element={
+            <ProtectedRoute
+              isAuthenticated={isAuthenticated}
+              isLoading={isLoading}
+            />
+          }
+        >
           <Route element={<Layout />}>
-            <Route path='/' element={<Notes />} />
-            <Route path='/archive' element={<Archive />} />
-            <Route path='/trash' element={<Trash />} />
+            <Route path='/' element={<NotesPage />} />
+            <Route path='/archive' element={<ArchivePage />} />
+            <Route path='/trash' element={<TrashPage />} />
           </Route>
         </Route>
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
