cd401cbb992058da04a1d739715b4c0e2062a87d
Almer Tampus <almerflorestampus@gmail.com>
2023-11-11 08:59:47 +0800

    Refactor Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,23 +1,35 @@
-// import packages below
+// packages
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
-// import components below
-import Content from '@/components/Content';
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+// components
+import Container from './components/Container';
+import Content from './components/Content';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Layout: React.FC = () => {
   return (
-    <main className='main'>
+    <Container>
       <Header />
-      <Sidebar />
-      <Content>
-        <Outlet />
-      </Content>
-      <Footer />
-    </main>
+      <Box
+        as='section'
+        display='flex'
+        flexWrap='nowrap'
+        alignItems='stretch'
+        w='full'
+      >
+        <Sidebar />
+        <Box as='section' w='full'>
+          <Content>
+            <Outlet />
+          </Content>
+          <Footer />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
