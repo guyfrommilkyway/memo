c46245e3ad6a846122cf6b798b58263dcb922289
Almer Tampus <almerflorestampus@gmail.com>
2023-11-11 09:00:07 +0800

    Add Footer component

---
+++ src/components/Layout/components/Footer.tsx
@@ -0,0 +1,26 @@
+// packages
+import React from 'react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+const Footer: React.FC = () => {
+  return (
+    <Box as='footer' p={4}>
+      <Flex
+        direction='column'
+        justify='center'
+        alignItems='center'
+        gap={0}
+        h='100%'
+      >
+        <Text textAlign='center' fontSize={14}>
+          Built using Vite, React and Chakra UI.
+        </Text>
+        <Text textAlign='center' fontSize={14}>
+          Made from Figma. Deployed with Netlify.
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
