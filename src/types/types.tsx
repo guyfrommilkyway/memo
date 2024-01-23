34aaeabf03e704aa9349290a17aad68bfc199489
Almer Tampus <almerflorestampus@gmail.com>
2024-01-23 13:37:25 +0800

    Add MyIconType type

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,6 +1,8 @@
 // packages
 import { ReactNode } from 'react';
 import { RawDraftContentState } from 'draft-js';
+import { IconType } from 'react-icons';
 
 export type MyReactNode = ReactNode;
 export type MyRawDraftContentState = RawDraftContentState;
+export type MyIconType = IconType;
