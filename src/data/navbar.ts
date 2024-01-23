a0936c9064ea8804b25487d515197cf9a0eb5d3b
Almer Tampus <almerflorestampus@gmail.com>
2024-01-23 13:37:12 +0800

    Relocate NavItem interface

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,14 +1,7 @@
-// import assets below
-import { IconType } from 'react-icons';
+// assets
 import { FaRegNoteSticky } from 'react-icons/fa6';
 import { FiTrash2 } from 'react-icons/fi';
 import { BiArchiveIn } from 'react-icons/bi';
-
-interface NavItem {
-  name: string;
-  link: string;
-  icon: IconType;
-}
 
 export const NAVBAR: NavItem[] = [
   { name: 'Notes', link: '/', icon: FaRegNoteSticky },
