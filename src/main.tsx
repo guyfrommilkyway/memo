b5103459fbbb8912c6ccdff8168bad3d7968b85c
Almer Tampus <almerflorestampus@gmail.com>
2024-02-24 18:14:25 +0800

    chore: pass theme to ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import packages below
+// packages
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -8,17 +8,20 @@ import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer } from 'react-toastify';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-// import components below
+// components
 import App from './app/App.tsx';
 import LoaderScreen from '@/components/LoaderScreen';
 
-// import helpers below
+// helpers
 import { store, persistor } from './app/store';
 
-// import assets below
+// assets
 import '@/assets/styles/globals.scss';
 import '@/assets/vendors/draftjs.scss';
 import '../node_modules/react-toastify/dist/ReactToastify.min.css';
+
+// config
+import theme from '@/config/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -31,7 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     >
       <Provider store={store}>
         <PersistGate loading={<LoaderScreen />} persistor={persistor}>
-          <ChakraProvider>
+          <ChakraProvider theme={theme}>
             <Suspense fallback={<LoaderScreen />}>
               <HelmetProvider>
                 <App />
