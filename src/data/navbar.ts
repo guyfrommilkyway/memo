95e75c57851db3d806f771d6581071060762fb02
Almer Tampus <almerflorestampus@gmail.com>
2024-02-24 18:16:25 +0800

    chore: update icons

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,10 +1,8 @@
 // assets
-import { FaRegNoteSticky } from 'react-icons/fa6';
-import { FiTrash2 } from 'react-icons/fi';
-import { BiArchiveIn } from 'react-icons/bi';
+import { MdNoteAlt, MdDelete, MdArchive } from 'react-icons/md';
 
 export const NAVBAR: NavItem[] = [
-  { name: 'Notes', link: '/', icon: FaRegNoteSticky },
-  { name: 'Archive', link: '/archive', icon: BiArchiveIn },
-  { name: 'Trash', link: '/trash', icon: FiTrash2 },
+  { name: 'Notes', link: '/', icon: MdNoteAlt },
+  { name: 'Archive', link: '/archive', icon: MdArchive },
+  { name: 'Trash', link: '/trash', icon: MdDelete },
 ];
