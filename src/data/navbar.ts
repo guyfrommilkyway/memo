5e6236f8ae1e2a6f0e27aad91e8ecc37db4550fd
Almer Tampus <almerflorestampus@gmail.com>
2024-04-29 00:09:39 +0800

    chore: remove unnecessary file

--- src/data/navbar.ts
+++
@@ -1,8 +0,0 @@
-// assets
-import { MdNoteAlt, MdDelete, MdArchive } from 'react-icons/md';
-
-export const NAVBAR: NavItem[] = [
-  { name: 'Notes', link: '/', icon: MdNoteAlt },
-  { name: 'Archive', link: '/archive', icon: MdArchive },
-  { name: 'Trash', link: '/trash', icon: MdDelete },
-];
