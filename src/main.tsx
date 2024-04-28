20cded99bd195428d287de3820246afc020a54cf
Almer Tampus <almerflorestampus@gmail.com>
2024-04-28 20:24:49 +0800

    chore: temporary remove auth0

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer } from 'react-toastify';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
 // components
 import App from './app/App.tsx';
