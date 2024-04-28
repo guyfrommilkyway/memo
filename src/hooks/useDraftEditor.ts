b49b64e8a7e08a6b5935f9a08985990cf9b3dfbe
Almer Tampus <almerflorestampus@gmail.com>
2024-04-28 22:51:39 +0800

    feat: add useDraftEditor hook

---
+++ src/hooks/useDraftEditor.ts
@@ -0,0 +1,30 @@
+// packages
+import { useState } from 'react';
+import { EditorState, RichUtils, DraftHandleValue } from 'draft-js';
+
+import { renderEditorDefaultState } from '@/constants/editor-state';
+
+const useDraftEditor = () => {
+  const [editorState, setEditorState] = useState(renderEditorDefaultState);
+
+  const toggleInlineStyle = (inlineStyle: string) => {
+    setEditorState(prev => RichUtils.toggleInlineStyle(prev, inlineStyle));
+  };
+
+  const toggleBlockType = (blockType: string) => {
+    setEditorState(prev => RichUtils.toggleBlockType(prev, blockType));
+  };
+
+  const handleKeyCommand = (command: string, editorState: EditorState): DraftHandleValue => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  };
+
+  return { editorState, setEditorState, handleKeyCommand, toggleInlineStyle, toggleBlockType };
+};
+
+export default useDraftEditor;
