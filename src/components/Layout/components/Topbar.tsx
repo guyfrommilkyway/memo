8b823ccf092452f4377bd04ef73716f4305c39a2
Almer Tampus <almerflorestampus@gmail.com>
2024-04-30 20:44:12 +0800

    style: set z-index to 2

--- src/components/Layout/components/Topbar.tsx
+++ src/components/Layout/components/Topbar.tsx
@@ -15,7 +15,19 @@ const Topbar: React.FC<Props> = ({ onToggle }) => {
   const { isAuthenticated, isLoading, logout } = useAuth0();
 
   return (
-    <Flex as='header' pos='sticky' top={0} justify='space-between' align='center' w='full' h='80px' mb='md' px='md'>
+    <Flex
+      as='header'
+      pos='sticky'
+      top={0}
+      zIndex={2}
+      justify='space-between'
+      align='center'
+      w='full'
+      h='80px'
+      mb='md'
+      px='md'
+      bg='brand.100'
+    >
       <Flex align='center' cursor='pointer' userSelect='none' onClick={onToggle}>
         <MdMenu fill='#414141' size={24} />
       </Flex>
